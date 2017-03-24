03b1f291497107c97e5eb49c83bd636e69b8ad4b
manognyam <manu.mallela@gmail.com>
2017-03-24 13:53:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,11 @@
 console.log('Loaded!');
+
 //change the main text
 var element = document.getElementById('main-text');
 element.innerHTML='new value';
+
+//move image
+var img = document.getElementById('madi');
+img.onclick = function () {
+    img.style.marginleft = '100px';
+};
