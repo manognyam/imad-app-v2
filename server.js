5172c58e1fa942376fce430f9f34750209f38fb6
manognyam <manu.mallela@gmail.com>
2017-03-24 17:37:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleOne = {
+var articles = {
+    'article-one':{
     title: 'article-one',
     heading: 'Article One',
     date: '23rd March 2017',
@@ -13,7 +14,20 @@ var articleOne = {
             </p>
             <p>This is my first article. Here goes the content for my first article. This is my first article. Here goes the content for my first article. This is my first article. Here goes the content for my first article. This is my first article. Here goes the content for my first article. This is my first article. Here goes the content for my first article.
             </p>
-            <p>This is my first article. Here goes the content for my first article. This is my first article. Here goes the content for my first article. This is my first article. Here goes the content for my first article. This is my first article. Here goes the content for my first article. This is my first article. Here goes the content for my first article.`
+            <p>This is my first article. Here goes the content for my first article. This is my first article. Here goes the content for my first article. This is my first article. Here goes the content for my first article. This is my first article. Here goes the content for my first article. This is my first article. Here goes the content for my first article.</p>`
+    },
+    'article-two':{
+    title: 'article-two',
+    heading: 'Article Two',
+    date: '23rd March 2017',
+    content: `<p>This is my second article. Here goes the content for my second article.</p>`
+    },
+    'article-three':{
+    title: 'article-three',
+    heading: 'Article Three',
+    date: '23rd March 2017',
+    content: `<p>This is my third article. Here goes the content for my third article..</p>`
+    }
 };
 
 function createTemplate (data) {
@@ -25,7 +39,7 @@ function createTemplate (data) {
     var htmlTemplate = `
     <html>
     <head>
-        <title> ${title}</title>
+        <title>${title}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <link href="/ui/style.css" rel="stylesheet" />
     </head>
@@ -47,22 +61,13 @@ function createTemplate (data) {
 return htmlTemplate;
 }
 
-
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
- res.sendFile(createTemplate(articleOne));
-});
-
-app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+app.get('/:articleName', function (req, res) {
+    var articleName = req.param.articleName;
+ res.sendFile(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
